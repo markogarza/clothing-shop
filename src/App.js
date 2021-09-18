94df013a61634e0e2cbd81b4ff1c68e920c9df5f
Marco Garza <60402991+markogarza@users.noreply.github.com>
2021-09-18 14:30:15 -0400

    Adding Header component and styling for links

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/Homepage/Homepage';
 import Shop from './pages/Shop/Shop';
+import Header from './components/Header/Header';
 
 function App() {
 	return (
 		<div className="App">
+			<Header />
 			<Switch>
 				<Route exact path="/" component={Homepage} />
 				<Route path="/shop" component={Shop} />
